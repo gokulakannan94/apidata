82d22cf279d9c5cc6a31efdc33f1e1a4d94b9a5f
gokulakannan94 <59164494+gokulakannan94@users.noreply.github.com>
2020-07-18 15:37:02 +0530

    Bug fix
    Corrected some invalid data

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -2,7 +2,7 @@ function sendMail()
 {
   var email = document.getElementById("email").value;
   var link = "mailto:gokulakannan1151@gmail.com"+
-             "?cc=CCaddress@example.com"+
+             "?cc=gokulakannan1151@outlook.com"+
              "&subject=" + escape("Mail from github page")+
              "&body=" + escape("The sender of the mail is "+email);
   window.location.href = link;
